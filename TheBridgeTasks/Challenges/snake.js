3931fc4bdec9e58c7cb11b720fa1f288bb7ca550
JNBarrachina <jnb.pasadobinario@gmail.com>
2025-02-17 13:38:58 +0100

    SnakeGame: first functions

--- TheBridgeTasks/Challenges/snake.js
+++ TheBridgeTasks/Challenges/snake.js
@@ -1,14 +1,43 @@
 const start = document.getElementById("sBox")
 start.addEventListener("click", startGame);
+addEventListener("keydown", direction);
 
 function startGame(event) {
     let initID = event.target.id;
+    console.log(initID);
     let snakeStart = document.getElementById(initID);
     snakeStart.style.backgroundColor = "green";
+
+    movement(initID)
 }
 
-function startMove(){
+function movement(initID) {
+    // const nextCell = ...
     setTimeout(function () {
-        
+        document.getElementById(initID).style.backgroundColor = "white";
+        initID++
+        document.getElementById(initID).style.backgroundColor = "green";
+        movement();
     }, 800);
+}
+
+function direction(event){
+    let dir = event.key;
+
+    switch (dir) {
+        case "ArrowUp":
+            alert(dir);
+            break;
+        case "ArrowDown":
+            alert(dir);
+            break;
+        case "ArrowLeft":
+            alert(dir);
+            break;
+        case "ArrowRight":
+            alert(dir);
+            break;
+        default:
+            break;
+    }
 }
