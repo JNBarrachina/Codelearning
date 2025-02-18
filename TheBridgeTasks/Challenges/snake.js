f7eca86d12709460e5997c4252cac883bf570155
JNBarrachina <jnb.pasadobinario@gmail.com>
2025-02-18 13:47:34 +0100

    Adding some styles and more features

--- TheBridgeTasks/Challenges/snake.js
+++ TheBridgeTasks/Challenges/snake.js
@@ -3,13 +3,13 @@ start.addEventListener("click", startGame);
 addEventListener("keydown", direction);
 
 let mHead = 1;
+let speed = 200;
+let points = 0;
 
 function startGame(event) {
     let initID = parseInt(event.target.id);
     let snake = [];
-    snake.push(initID, initID - 1, initID - 2);
-
-    console.log(snake);
+    snake.push(initID, initID - 1);
 
     paintSnake(snake);
     movement(snake);
@@ -18,43 +18,44 @@ function startGame(event) {
 function movement(snake) {
 
     setInterval(() => {
-        deleteSnake(snake);
+    deleteSnake(snake);
     for (let e = snake.length - 1; e > 0; e--){
         snake[e] = snake[e - 1];
     }
     snake[0] = snake [0] + mHead;
     paintSnake(snake);
-    }, 600);
+    }, speed);
 
 }
 
 function deleteSnake(snake) {
     for (let d = 0; d < snake.length; d++){
-        document.getElementById(snake[d]).style.backgroundColor = "white";
+        document.getElementById(snake[d]).style.backgroundColor = "#273849";
     }
 
     return;
 }
 
 function paintSnake(snake) {
-
+    
     for (let p = 0; p < snake.length; p++){
-        document.getElementById(snake[p]).style.backgroundColor = "green";
+        document.getElementById(snake[p]).style.backgroundColor = "greenyellow";
     }
-
+    
     return;
 }
+
 
 function direction(event){
     let dir = event.key;
 
     switch (dir) {
         case "ArrowUp":
-            mHead = -10;
+            mHead = -20;
             movement(snake);
             break;
         case "ArrowDown":
-            mHead = 10;
+            mHead = 20;
             movement(snake);
             break;
         case "ArrowLeft":
@@ -68,4 +69,11 @@ function direction(event){
         default:
             break;
     }
+}
+
+function food(){
+}
+
+function gameover(){
+    alert("YOU LOOSE")
 }
