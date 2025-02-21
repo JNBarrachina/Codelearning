1bd51c74483e1df686669dc2f1aa0ccd1999de93
JNBarrachina <jnb.pasadobinario@gmail.com>
2025-02-21 20:06:04 +0100

    Challenge completed: PokemonList

--- TheBridgeTasks/Challenges/pokemon.js
+++ TheBridgeTasks/Challenges/pokemon.js
@@ -1,3 +1,6 @@
+const searchPoke = document.getElementById("inputP");
+searchPoke.addEventListener("input", filterPokemons);
+
 const pokemons = [
     {
     name: "Pikachu",
@@ -29,39 +32,39 @@ const pokemons = [
     },
 ];
 
-function showAllList(){
+const pokeBox = document.getElementById("pokeFinder");
+const pokemonsUL = document.createElement("ul");
+pokemonsUL.setAttribute("class", "pokemonList");
+pokemonsUL.setAttribute("id", "pokeList");
+pokeBox.appendChild(pokemonsUL);
 
-    pokemons.forEach(pokemon => {
-        const allPokemons = document.getElementById("pokeList");
+filterPokemons();
+
+function filterPokemons(){
+    let s = document.getElementById("inputP").value.toLowerCase();
+
+    let filteredPokemons = pokemons.filter((pokemon) => {
+        return pokemon.name.toLowerCase().includes(s);
+    });
+
+    pokemonsUL.innerHTML = "";
+    showPokemons(filteredPokemons, pokemonsUL);
+}
+
+function showPokemons(filteredPokemons, pokemonsUL) {
+
+    filteredPokemons.forEach(pokemon => {
         const pokeListItem = document.createElement("li");
         pokeListItem.setAttribute("class", "pokeItem")
 
         const pokeName = document.createElement("p");
         pokeName.setAttribute("class", "pname");
+        pokeName.innerHTML = "<b>Pokemon: </b>" + pokemon.name;
         const pokeType = document.createElement("p");
         pokeType.setAttribute("class", "ptype");
-        pokeName.innerHTML = "<b>Pokemon: </b>" + pokemon.name;
         pokeType.innerHTML = "<b>Type: </b>" + pokemon.type;
     
         pokeListItem.append(pokeName, pokeType);
-        allPokemons.appendChild(pokeListItem);
+        pokemonsUL.appendChild(pokeListItem);
     });
-}
-
-showAllList();
-
-const searchPoke = document.getElementById("inputP");
-searchPoke.addEventListener("input", showPokemon);
-
-function showPokemon(){
-    let s = document.getElementById("inputP").value;
-
-    pokemons.forEach(pokemon => {
-        if (pokemon.name.includes(s)){
-            findedPokemons.push(pokemon);
-        }
-    });
-
-    console.log(findedPokemons);
-    return findedPokemons;
 }
