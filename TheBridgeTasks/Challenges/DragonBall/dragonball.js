8eeadecd8bd412981c946cb231e9548fb8ba3d2b
JNBarrachina <jnb.pasadobinario@gmail.com>
2025-02-26 23:34:44 +0100

    Added characters descriptions, some new styles and a new sticky header

--- TheBridgeTasks/Challenges/DragonBall/dragonball.js
+++ TheBridgeTasks/Challenges/DragonBall/dragonball.js
@@ -1,30 +1,45 @@
+
 fetch("https://dragonball-api.com/api/characters?&limit=1000")
     .then((response) => response.json())
     .then((data) => {
-        data.items.forEach(ch => {
-            const nameChar = ch.name;
-            const kiChar = ch.ki;
-            const raceChar = ch.race;
-            const genChar = ch.gender;
-            const imgChar = ch.image;
+        data.items.forEach(character => {
+            const idChar = character.id;
+            const nameChar = character.name;
+            const kiChar = character.ki;
+            const raceChar = character.race;
+            const genChar = character.gender;
+            const imgChar = character.image;
 
-            createCharac(nameChar, kiChar ,raceChar, genChar, imgChar);
+            createCharac(idChar, nameChar, kiChar ,raceChar, genChar, imgChar);
         });
+
+        allData = data.items;
     })
     .catch((error) => {
         console.error("Error en la solicitud:", error);
     });
 
-const charList = document.getElementById("dbList");
 
-function createCharac(nameChar, kiChar, raceChar, genChar, imgChar) {
+const charactersGrid = document.getElementById("dbList")
+charactersGrid.addEventListener("mouseover", selectDescript);
+const charDescript = document.createElement("p");
+charDescript.setAttribute("class", "pDescription");
+let allData;
+
+
+function createCharac(idChar, nameChar, kiChar, raceChar, genChar, imgChar) {
     const boxChar = document.createElement("article");
     boxChar.setAttribute("class", "dbCharacter");
+    boxChar.setAttribute("id", `box${idChar}`);
 
     const charImg = document.createElement("img");
     charImg.src = imgChar;
+
     const charName = document.createElement("p");
+    charName.setAttribute("id", idChar);
+    charName.setAttribute("class", "characterName");
     charName.innerText = nameChar;
+
     const charKi = document.createElement("p");
     charKi.innerText = kiChar;
     const charRace = document.createElement("p");
@@ -33,5 +48,19 @@ function createCharac(nameChar, kiChar, raceChar, genChar, imgChar) {
     charGen.innerText = genChar;
 
     boxChar.append(charImg, charName, charKi, charRace, charGen);
-    charList.append(boxChar);
+    charactersGrid.append(boxChar);
 }
+
+function selectDescript(event){
+    const idDescript = event.target.id;
+    charDescript.style.visibility = "hidden";
+    charDescript.innerText = "";
+    showDescript(allData, idDescript);
+}
+
+function showDescript(allData, idDescript){
+    charDescript.innerText = allData[idDescript-1].description;
+    charactersGrid.appendChild(charDescript);
+    charDescript.style.visibility = "visible";
+}
+
