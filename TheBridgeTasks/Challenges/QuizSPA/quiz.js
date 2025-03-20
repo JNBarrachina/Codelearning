4fec598610d988bdfdaf59642868c564d4f909c3
Juan Narros <jnb.pasadobinario@gmail.com>
2025-03-20 21:48:05 +0100

    Quiz challenge: showing questions, answers and saving data

--- TheBridgeTasks/Challenges/QuizSPA/quiz.js
+++ TheBridgeTasks/Challenges/QuizSPA/quiz.js
@@ -1,13 +1,20 @@
 const API_URL = "https://opentdb.com/api.php?amount=10&type=multiple";
 const quizButton = document.getElementById("buttonQuiz");
 quizButton.addEventListener("click", getQuiz);
+const nextQ = document.getElementById("buttonNext");
+nextQ.addEventListener("click", () => nextQuestion());
+const showResults = document.getElementById("buttonResults");
+showResults.addEventListener("click", () => checkResults(quiz, finalAnswers));
 
 let quiz;
 function getQuiz(){
+
+
     fetch(API_URL)
     .then((response) => response.json())
     .then((data) => {        
         quiz = data.results;
+        console.log(quiz);
         showQuestion(quiz);
     })
 
@@ -19,8 +26,8 @@ function getQuiz(){
 const showQ = document.getElementById("boxQuestions");
 
 let numQuestion = 0;
-async function showQuestion(quiz){
 
+async function showQuestion(quiz){
     const arrayAnswers = [];
     arrayAnswers.push(quiz[numQuestion].correct_answer);
     quiz[numQuestion].incorrect_answers.forEach(incorrectAns => {
@@ -28,6 +35,16 @@ async function showQuestion(quiz){
     });
 
     await shuffleAnswers(arrayAnswers);
+
+    let boxAnswers = "";
+    arrayAnswers.forEach(answer => {
+        boxAnswers += `
+        <section class="singleAnswer">
+            <input type="radio" name="question${numQuestion}" id="${answer}" onclick="saveAnswer(event, numQuestion)"/>
+            <label for="questionChoice${arrayAnswers.indexOf(answer)}">${answer}</label>
+        </section>
+        `
+    });
 
     showQ.innerHTML = 
     `<article class="singleQuestion" id="${numQuestion}">
@@ -37,23 +54,56 @@ async function showQuestion(quiz){
                 <fieldset>
                 <legend>${quiz[numQuestion].question}</legend>
                 <section class="selectAnswers">
-                   <section class="singleAnswer">
-                        <input type="radio" id="contactChoice1" name="contact" value="email" />
-                        <label for="contactChoice1">Correo electrónico</label>
-                   </section>
+                   ${boxAnswers}
                 </section>
                 </fieldset>
             </form>
         </section>
     </article>
     `;
-
-    numQuestion++;
 }
 
 function shuffleAnswers(arrayAnswers){
+    //* Algoritmo de FIsher-Yates.
 
+    for (let i = arrayAnswers.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arrayAnswers[i], arrayAnswers[j]] = [arrayAnswers[j], arrayAnswers[i]];
+    }
+
+    return arrayAnswers;
 }
 
-const nextQuestion = document.getElementById("buttonNext");
-nextQuestion.addEventListener("click", () => showQuestion(quiz));
+let finalAnswers = [];
+function saveAnswer(event, idAnswer) {
+    const answerQuestion = event.target.id;
+
+    if (numQuestion == quiz.length - 1){
+        nextQ.style.display = "none";
+        showResults.style.display = "inline";
+    }
+
+    if (finalAnswers.length == 0){
+        finalAnswers.push(answerQuestion);
+    }
+    else{
+        finalAnswers[idAnswer] = answerQuestion;
+    }
+
+    console.log(finalAnswers);
+}
+
+
+function nextQuestion(){
+    numQuestion++;
+
+    console.log(numQuestion, quiz.length - 1);
+    
+    showQuestion (quiz);
+}
+
+function checkResults(quiz, finalAnswers){
+    console.log("QUIZ FINALIZADO");
+}
+
+
