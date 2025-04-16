1256e8f628f1774ab5ab1776b48cd21d659004be
JNBarrachina <jnb.pasadobinario@gmail.com>
2025-04-16 13:52:03 +0200

    CRUD DELETE

--- TheBridgeTasks/CRUD (ExpressJS)/index.js
+++ TheBridgeTasks/CRUD (ExpressJS)/index.js
@@ -6,18 +6,44 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const animals = [
-    {name: "Gato", strength: 3},
-    {name: "Elefante", strength: 8},
-    {name: "Murciélago", strength: 1}
+let animals = [
+    {id: 1, name: "Gato", strength: 3},
+    {id: 2, name: "Elefante", strength: 8},
+    {id: 3, name: "Murciélago", strength: 1},
+    {id: 4, name: "Perro", strength: 4},
+    {id: 5, name: "Rino", strength: 10},
+    {id: 6, name: "Cuca", strength: 1},
+    {id: 7, name: "Serpiente", strength: 3},
+    {id: 8, name: "Yog-Sothoth", strength: "?"}
 ]
+
+let currentID = animals.length + 1;
+
+app.use(express.static('public'));
 
 app.get("/animals", (req, res) => {
     res.send(animals);
 });
 
 app.post("/animals", (req, res) => {
-    animals.push(req.body);
+    const newAnimal = {id: currentID, name: req.body.name, strength: req.body.strength}
+    currentID++;
+    
+    animals.push(newAnimal);
+});
+
+app.delete("/animals/:id", (req, res) => {
+    const idToDelete = parseInt(req.params.id);
+    
+    const filteredAnimals = animals.filter((animal) => animal.id != idToDelete);
+
+    if (filteredAnimals.length == animals.length){
+        res.send("No se ha encontrado ningún elemento a borrar");
+    }
+    else{
+        animals = filteredAnimals;
+        res.send("Elemento borrado")
+    }
 });
 
 app.listen(port, () => {
--- TheBridgeTasks/CRUD (ExpressJS)/public/animals.js
+++ TheBridgeTasks/CRUD (ExpressJS)/public/animals.js
@@ -1,4 +1,6 @@
+const BASE_URL = "http://localhost:5376";
 
+const mainPage = document.getElementById("mainpage");
 const postAnimal = document.getElementById("btnAddAnimal");
 postAnimal.addEventListener("click", checkData);
 const btnAnimals = document.getElementById("showAnimals");
@@ -31,7 +33,7 @@ function checkData(event){
 }
 
 function addAnimal(newAnimal){
-    fetch("http://localhost:5376/animals", {
+    fetch(`${BASE_URL}/animals`, {
         method: "POST", // or 'PUT'
         body: JSON.stringify(newAnimal), // data can be `string` or {object}!
         headers: {
@@ -47,29 +49,93 @@ function addAnimal(newAnimal){
     .then((response) => console.log("Success:", response));
 
     message.innerText = "Animal anadido correctamente";
+    getAllAnimals();
 }
 
 function getAllAnimals(){
-    fetch("http://localhost:5376/animals")
+    fetch(`${BASE_URL}/animals`)
     .then((response) => response.json())
     .then((data) => {
         animalsSection.innerHTML = "";
-        const animalsList = document.createElement("ul");
     
         data.forEach(animal => {
-            const singleAnimal = document.createElement("li");
+            const singleAnimal = document.createElement("button");
+            singleAnimal.setAttribute("class", "animalItem");
+            singleAnimal.setAttribute("id", `${animal.id}`);
+            singleAnimal.addEventListener("click", ()  => confirmRemove(animal));
+            singleAnimal.addEventListener("mouseover", () => changeBtnText(singleAnimal.id));
             singleAnimal.innerText = `${animal.name}: ${animal.strength} de fuerza`;
 
-            animalsList.append(singleAnimal);
+            animalsSection.append(singleAnimal);
         });
-        animalsSection.append(animalsList);
-    })
+
+        animalsSection.style.visibility = "visible";
+    }) 
 
     .catch((error) => {
         console.error("Error en la solicitud:", error);
     });
 }
 
+const confirmRemove = (animal) => {
+    const removeBox = document.createElement("div");
+    removeBox.setAttribute("class", "removeBox");
+    removeBox.setAttribute("id", "removeWindow");
 
+    const removeMessage = document.createElement("p");
+    removeMessage.innerText = `¿Quieres eliminar ${animal.name} definitivamente?`;
 
+    const removeBtns = document.createElement("div");
+    removeBtns.setAttribute("class", "removeBtnsBox");
+    const removeYes = document.createElement("button");
+    removeYes.addEventListener("click", () => removeAnimal(animal.id))
+    removeYes.innerText = "SÍ";
+    const removeNo = document.createElement("button");
+    removeNo.addEventListener("click", () => closeRemoveWindow());
+    removeNo.innerText = "NO";
+    removeBtns.append(removeYes, removeNo);
+    
 
+    removeBox.append(removeMessage, removeBtns);
+    mainPage.append(removeBox);
+}
+
+const removeAnimal = (removedAnimal) => {
+    fetch(`${BASE_URL}/animals/${removedAnimal}`, {
+        method: "DELETE", // or 'PUT'
+        headers: {
+        "Content-Type": "application/json",
+        },
+    })
+
+    
+    .then((res) => res.json())
+    
+    .catch((error) => console.error("Error:", error))
+    
+    .then((response) => console.log("Success:", response));
+
+    closeRemoveWindow();
+    getAllAnimals();
+}
+
+const changeBtnText = (id) => {
+    const btnChangeText =  document.getElementById(`${id}`);
+
+    const originalText = btnChangeText.innerText;
+    btnChangeText.addEventListener("mouseout", () => resetBtnText(btnChangeText, originalText));
+
+    const newBtnText = btnChangeText.innerText.split(":");
+    btnChangeText.innerText = "";
+
+    btnChangeText.innerText = `¿Eliminar ${newBtnText[0]}?`;
+}
+
+const resetBtnText = (btnChangeText, originalText) => {
+    btnChangeText.innerText = `${originalText}`;
+}
+
+const closeRemoveWindow = () => {
+    const removedWindow = document.getElementById("removeWindow");
+    removedWindow.remove();
+}
