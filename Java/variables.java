d3c0835a9a03f5e680f36508c520a03442c317fc
Juan Narros <jnb.pasadobinario@gmail.com>
2025-12-14 12:58:31 +0100

    Java First Steps

---
+++ Java/variables.java
@@ -0,0 +1,25 @@
+// Comentario de una línea
+/** Comentario de documentación */
+
+// ? VARIABLES
+
+// * Declaración: tipo + nombre */
+int a = 10;
+float b = 3.14f;
+char c = 'A';
+boolean e = true;
+
+// ? Las variables no inicializadas reciben un valor por defecto: 0, 0.0f, '\0',
+
+// * Variables locales y globales
+
+void main() {
+    int num = 10; // Variable local
+    System.out.println(num);
+}
+
+int num = 10; // Variable global
+
+// * Constantes
+
+final int NUM = 10;
